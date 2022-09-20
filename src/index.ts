6da604b238fba6ca39660eaec05a02242bfd9877
Cristi Petre <driimus@vivaldi.net>
2022-09-20 11:33:15 +0100

    feat: auto library mixin

---
+++ src/index.ts
@@ -0,0 +1,58 @@
+type Constructor<T = {}, Arguments extends unknown[] = any[]> = new (
+  ...args: Arguments
+) => T;
+
+type ArrayEntry<T> = T extends Array<infer U> ? U : never;
+
+type TupleToUnion<T extends unknown[]> = T[number];
+
+type UnionToIntersection<T> = (
+  T extends unknown ? (k: T) => void : never
+) extends (k: infer I) => void
+  ? I
+  : never;
+
+export function autoMix<T, Base extends Constructor>(
+  apiModule: { [ModuleEntry: string]: T },
+  baseApi: Base
+) {
+  return mixApis(
+    Object.values(apiModule).filter<Extract<T, Base>>(derivesFromBase(baseApi)),
+    baseApi
+  );
+}
+
+export function mixApis<T extends Constructor[], Base extends Constructor>(
+  mixins: T,
+  baseApi: Base
+): Constructor<
+  UnionToIntersection<InstanceType<TupleToUnion<T>>>,
+  ConstructorParameters<Base>
+> {
+  const mixed = class extends baseApi {};
+
+  applyMixins(mixed, mixins);
+
+  return mixed;
+}
+
+function applyMixins<T extends Constructor[]>(
+  derivedCtor: any,
+  constructors: T
+): asserts derivedCtor is Constructor<
+  UnionToIntersection<InstanceType<ArrayEntry<T>>>
+> {
+  for (const baseCtor of constructors)
+    for (const name of Object.getOwnPropertyNames(baseCtor.prototype))
+      Object.defineProperty(
+        derivedCtor.prototype,
+        name,
+        Object.getOwnPropertyDescriptor(baseCtor.prototype, name) ||
+          Object.create(null)
+      );
+}
+
+const derivesFromBase =
+  <T extends Constructor>(baseClass: T) =>
+  <U>(obj: U): obj is Extract<U, T> =>
+    baseClass.prototype.isPrototypeOf(obj);
