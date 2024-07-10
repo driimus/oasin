1326f508f2273312f83ffdfbfaddb2135ea7dd43
Cristian Petre <driimus@vivaldi.net>
2024-07-10 23:41:35 +0100

    chore(setup): use lefthook (#56)
    * chore(setup): replace lint-staged
    
    * chore(deps): update dependency eslint-plugin-unicorn to v54
    
    * chore(deps): update pnpm to v9.5.0
    
    * style(eslint): ignore any configs
    
    * chore(deps): remove `lint-staged`
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vitest/config';
+import tsConfig from './tsconfig.json' with { type: 'json' };
 
 export default defineConfig({
   test: {
     globals: true,
     setupFiles: ['jest-extended/all'],
     silent: true,
-    coverage: { enabled: true },
+    coverage: { enabled: true, include: tsConfig.include },
   },
 });
